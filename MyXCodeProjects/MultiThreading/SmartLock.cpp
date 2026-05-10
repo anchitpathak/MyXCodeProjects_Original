f76d235485d87dff7c85e9a7962eca71a73124d0
anchitpathak <anchitpathak93@gmail.com>
2026-05-10 21:31:01 +0530

    Reverse

--- MyXCodeProjects/MultiThreading/SmartLock.cpp
+++ MyXCodeProjects/MultiThreading/SmartLock.cpp
@@ -21,19 +21,19 @@ void writer() {
     std::unique_lock<std::shared_mutex> lock(smtx); // exclusive write
     data.push_back(6);
     std::cout << "Writer: added 6" << std::endl;
-    std::cout<<"Anchit Pathak";
+    std::cout<<"AnchitPathak";
 }
 
-//int main() {
-//    std::thread r1(reader, 1);
-//    std::thread r2(reader, 2);
-//    std::thread w1(writer);
-//    std::thread r3(reader, 3);
-//
-//    r1.join();
-//    r2.join();
-//    w1.join();
-//    r3.join();
-//
-//    return 0;
-//}
+int main() {
+    std::thread r1(reader, 1);
+    std::thread r2(reader, 2);
+    std::thread w1(writer);
+    std::thread r3(reader, 3);
+
+    r1.join();
+    r2.join();
+    w1.join();
+    r3.join();
+
+    return 0;
+}
